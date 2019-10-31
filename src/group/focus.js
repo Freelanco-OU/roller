51cfa7e76b7f9ba1d8263dc6dcaac2c5a51a52c8
Kapelianovych Yevhen <zhenja.96.lemok@ukr.net>
2019-10-31 23:17:37 +0200

    Add .prettierrc.json settings. Transfer Focus to Flow annotations. Add comma-dangle rule.

--- src/group/focus.js
+++ src/group/focus.js
@@ -1,28 +1,41 @@
+// @flow
+
 const {
   ELEMENT_Z_INDEX,
   ELEMENT_TRANSITION,
   ELEMENT_POSITION,
-  ELEMENT_BACKGROUND_COLOR
+  ELEMENT_BACKGROUND_COLOR,
 } = require('../constants.js')
 const { wait } = require('../utils/helpers.js')
+
+type FocusOptions = {
+  beforeHighlight?: (node: HTMLElement, wait: typeof wait) => Promise<void>,
+  afterHighlight?: (node: HTMLElement, wait: typeof wait) => Promise<void>,
+}
 
 /**
  * Main class that represent highlighter.
  * Also can start guide in the page.
  */
 class Focus {
+  node: HTMLElement
+  _options: FocusOptions
+  _initialStyles: CSSStyleDeclaration
+
   /**
    * Construct `Focus` instance.
-   * @param {HTMLElement | String} element
-   * @param {{
-   *  beforeHighlight?: (node: HTMLElement, wait: (seconds: Number | String) => Promise<void>) => Promise<void>,
-   *  afterHighlight?: (node: HTMLElement, wait: (seconds: Number | String) => Promise<void>) => Promise<void>
-   * }} [options]
    */
-  constructor(element, options) {
+  constructor(element: HTMLElement | string, options?: FocusOptions) {
     if (typeof element === 'string') {
-      /** @type {HTMLElement} */
-      this.node = document.querySelector(element)
+      const el = document.querySelector(element)
+
+      if (el) {
+        this.node = el
+      } else {
+        throw new Error(
+          'Element that need to be highlighted is not defined on the page!'
+        )
+      }
     } else {
       this.node = element
     }
@@ -36,36 +49,33 @@ class Focus {
       await this._options.beforeHighlight(this.node, wait)
     }
 
-    if (this.node) {
-      // Remember initial styles of element. Will be needed in canceling focus.
-      this._initialStyles = getComputedStyle(this.node)
+    // Remember initial styles of element. Will be needed in canceling focus.
+    this._initialStyles = (getComputedStyle(this.node): CSSStyleDeclaration)
 
-      let parent = this.node
-      let backgroundColor = this._initialStyles.backgroundColor
-      while (!backgroundColor || backgroundColor === 'rgba(0, 0, 0, 0)') {
-        parent = parent.parentElement
-        if (parent) {
-          backgroundColor = getComputedStyle(parent).backgroundColor
-        } else {
-          backgroundColor = ELEMENT_BACKGROUND_COLOR
-        }
+    let parent = this.node
+    let backgroundColor = this._initialStyles.backgroundColor
+    while (!backgroundColor || backgroundColor === 'rgba(0, 0, 0, 0)') {
+      parent = parent && parent.parentElement
+      if (parent) {
+        backgroundColor = (getComputedStyle(parent): CSSStyleDeclaration)
+          .backgroundColor
+      } else {
+        backgroundColor = ELEMENT_BACKGROUND_COLOR
       }
-
-      // Styles need to be set inline, because program may add additional styles.
-      this.node.style.transition = ELEMENT_TRANSITION
-      this.node.style.zIndex = `${ELEMENT_Z_INDEX}`
-      this.node.style.backgroundColor = backgroundColor
-      if (
-        this._initialStyles.position === 'initial' ||
-        this._initialStyles.position === 'static'
-      ) {
-        this.node.style.position = ELEMENT_POSITION
-      }
-
-      this.node.classList.add('roller-highlighted-element')
-    } else {
-      console.error('No element to highlight!')
     }
+
+    // Styles need to be set inline, because program may add additional styles.
+    this.node.style.transition = ELEMENT_TRANSITION
+    this.node.style.zIndex = `${ELEMENT_Z_INDEX}`
+    this.node.style.backgroundColor = backgroundColor
+    if (
+      this._initialStyles.position === 'initial' ||
+      this._initialStyles.position === 'static'
+    ) {
+      this.node.style.position = ELEMENT_POSITION
+    }
+
+    this.node.classList.add('roller-highlighted-element')
 
     if (this._options.afterHighlight) {
       await this._options.afterHighlight(this.node, wait)
