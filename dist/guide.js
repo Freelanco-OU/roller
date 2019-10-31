da4c5d3481b886e1887e706b229c251cad369b04
Kapelianovych Yevhen <zhenja.96.lemok@ukr.net>
2019-10-31 21:30:10 +0200

    Initial commit.

---
+++ dist/guide.js
@@ -0,0 +1,155 @@
+//      
+
+                                                 
+
+const Controller = require('./controller.js')
+// eslint-disable-next-line no-unused-vars
+const Overlay = require('./group/overlay.js')
+// eslint-disable-next-line no-unused-vars
+const Focus = require('./group/focus.js')
+// eslint-disable-next-line no-unused-vars
+const Popover = require('./group/popover.js')
+const Roller = require('./roller.js')
+const Tip = require('./tip.js')
+
+                               
+                          
+                          
+                          
+                          
+                                              
+                                                  
+                                                  
+                                                  
+                                                 
+ 
+
+                     
+                           
+                    
+                                                           
+                      
+                     
+ 
+
+/** Class that creates controls elements in popover, when guide is started. */
+class Guide {
+                
+                      
+                           
+                   
+                        
+                        
+                         
+                 
+            
+
+  /**
+   * Defines `options` for *Guide* object.
+   * `overlay` - Common overlay for guide object.
+   * `onDone` - Executes when user has finished guide.
+   * `onSkip` - Executes when user has finished guide.
+   */
+  constructor(options              ) {
+    this._steps = options.groups.length
+    this._currentStep = 0
+    this._groups = options.groups
+    this._overlay = options.overlay || new Overlay()
+    this._onDone = options.onDone
+    this._onSkip = options.onSkip
+    if (options.buildTip) {
+      this._tip = options.buildTip(() => this.move(0), () => this.cancel())
+    }
+  }
+
+  /**
+   * Creates and configures common controller.
+   */
+  configure(options                          = {}) {
+    this._controller = new Controller({
+      onSkip: (event) => {
+        this.cancel()
+
+        if (this._onSkip) {
+          this._onSkip()
+        }
+      },
+      onPrev: (event) => {
+        this.cancel()
+
+        if (this._currentStep > 0) {
+          this.move(--this._currentStep)
+        }
+      },
+      onNext: (event) => {
+        this.cancel()
+
+        if (this._currentStep === this._groups.length - 1 && this._onDone) {
+          this._onDone()
+        } else {
+          this.move(++this._currentStep)
+        }
+      },
+      skipButtonText: options.skipButtonText || 'Skip',
+      prevButtonText: options.prevButtonText || 'Previous',
+      nextButtonText: options.nextButtonText || 'Next',
+      doneButtonText: options.doneButtonText || 'Done',
+      footerStyle: options.footerStyle,
+      skipButtonStyle: options.skipButtonStyle,
+      prevButtonStyle: options.prevButtonStyle,
+      nextButtonStyle: options.nextButtonStyle,
+      doneButtonStyle: options.doneButtonStyle,
+      steps: this._steps,
+      currentStep: this._currentStep
+    })
+
+    this._roller = new Roller({
+      onOverlayClick: () => {}
+    })
+
+    return this
+  }
+
+  /**
+   * Move to next group.
+   * `step` - Number of step. Must be in range from 0 to length of `groups - 1`.
+   */
+  move(step        ) {
+    if (step >= 0 && this._steps > step) {
+      this._currentStep = step
+
+      const group = this._groups[step]
+      if (!group.overlay) {
+        // Set global overlay on start. It is used for making hight contrast with highlighted element.
+        if (this._currentStep === 0) {
+          group.overlay = this._overlay
+        }
+        group.overlay = this._overlay
+      }
+
+      this._controller.updatePosition(step)
+      if (group.popover) {
+        group.popover._addController(this._controller)
+      }
+
+      this._roller.highlight(group)
+    }
+  }
+
+  /** Starts guide. */
+  start() {
+    if (this._tip) {
+      this._tip.show()
+    } else {
+      this.move(0)
+    }
+  }
+
+  /** Cancel guide. */
+  cancel() {
+    const group = this._groups[this._currentStep]
+    this._roller.unHighlight(group)
+  }
+}
+
+module.exports = Guide
---
+++ dist/index.js
@@ -0,0 +1,17 @@
+//      
+
+const Focus = require('./group/focus')
+const Roller = require('./roller')
+const Popover = require('./group/popover')
+const Overlay = require('./group/overlay')
+const Guide = require('./guide')
+const Tip = require('./tip')
+
+module.exports = {
+  Focus,
+  Roller,
+  Popover,
+  Overlay,
+  Guide,
+  Tip
+}
