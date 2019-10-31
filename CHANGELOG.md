9059417b322b40fd033051ea131614f5b50e28c4
Kapelianovych Yevhen <zhenja.96.lemok@ukr.net>
2019-10-31 22:29:20 +0200

    Add dist/ to .gitignore. Edit changelog. Remove dist/ from git index. Change margin-bottom of description of the popover.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,9 @@
-# 0.1.0
+# 0.2.0
+
+- Transfer source code to Flow.
+- Fix margin-bottom in description of `Popover`.
+
+## 0.1.0
 
 - Creating `Roller` class for highlighting single element on the page.
 - Creating `Guide` class for creating intro.
--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
-//      
+// @flow
 
 const Focus = require('./group/focus')
 const Roller = require('./roller')
 const Popover = require('./group/popover')
 const Overlay = require('./group/overlay')
 const Guide = require('./guide')
-const Tip = require('./tip')
 
 module.exports = {
   Focus,
   Roller,
   Popover,
   Overlay,
-  Guide,
-  Tip
+  Guide
 }
